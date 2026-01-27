9ffc8de345a5673dd85702f2418289446a390830
Ex <ex@G401-CC0110.local>
2026-01-27 18:41:03 +0800

    P

--- app/api/roadmap/[userId]/route.ts
+++ app/api/roadmap/[userId]/route.ts
@@ -1,8 +1,18 @@
-import { NextRequest } from "next/server";
+import prisma from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
 
 export const POST = async (
   req: NextRequest,
   context: { params: { userId: string } },
 ) => {
-  // const
+  const { userId } = await context.params;
+  const body = await req.json();
+  const createdRoadmap = await prisma.roadmap.create({
+    data: {
+      userId,
+      title: body.title,
+      purpose: body.purpose,
+    },
+  });
+  return NextResponse.json(createdRoadmap);
 };
