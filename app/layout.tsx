eae9bf28f6ca50daec1a9f6e416cec875182c43d
25hp3519 User <25hp3519@Users-iMac-2.local>
2026-03-07 10:58:48 +0800

    P

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "../providers/AuthProviders";
-
+  
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
